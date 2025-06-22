d2b383b8ba54e53369d6ede5104ff33e995ea119
EmersonOsantos <129695285+EmersonOsantos@users.noreply.github.com>
2025-06-22 10:55:27 -0400

    Update edital-medio.cy.ts

--- cypress/e2e/edital-medio.cy.ts
+++ cypress/e2e/edital-medio.cy.ts
@@ -1,80 +1,93 @@
 import { getCurrentDateTime } from '../helpers/date.helper';
+import 'cypress-iframe';
+import 'cypress-real-events';
+//chmod +x node_modules/.bin/cypress  // Torna o comando cypress executável
+// npx cypress open  // Abre a interface gráfica do Cypress
 
-describe('Sistema Integrado de Gestão para Fundações de Amparo a Pesquisas', () => {
+describe('Criação de Edital Médio - Grupo 12', () => {
   beforeEach(() => {
-    // Gancho em nível raíz
-    // executa antes de realizar cada teste(it)
     cy.typelogin(
-      'https://novo-sig.ledes.net',// [URL do sistema]
-      'grupo12_gestor@sig.com', // [E-mail do usuário]
-      'Grupo12@sig', // [Senha do usuário]
-    ); //Acessa a página de login usando as credenciais do usuário e senha.
-  });
-  it.only('Realiza login no sistema e cria um edital medio', () => { //Teste edital medio, se houver mais de um teste, o it.only executa apenas esse teste.
-    cy.get('[data-cy="nav-group-edital"]').click(); //Clica na aba Editais
-    cy.get('[data-cy="nav-item-publicar-edital"]').click(); //Clica na opção Editais para acessar da página de Editais
-    cy.get('[data-cy="add-publicar-edital"]').click(); //Clica no botão "Adicionar" para criação de um novo Edital
-    cy.get('[data-cy="nome"]').type(
-      'Grupo-12 E.M. 012/2025 Eduã - Emerson', //Edite essa linha para preencher o nome do Edital
-      { delay: 0 },
-    ); //Preenche o campo "Nome" do Edital
-    cy.get('[data-cy="restricoes"]').click(); //Clica na aba Restrições para seguir para a página de Restrições
-    cy.get('[data-cy="definirDuracaoProjetoEmMeses"]').check(); //Marca a opção "Definir Duração do Projeto em Meses"
-    cy.get('[data-cy="duracaoProjetoEmMeses"]').type('6'); //Preenche o campo "Duração do Projeto em Meses com o valor 6"
-    cy.get('[data-cy="pesquisadorSubmeterVariasPropostas"]').check(); //Marca a opção "Pesquisador pode submeter várias propostas"
-    
-    cy.get('[data-cy="termo-de-aceite"]').click(); //Clica na aba Termo de Aceite para seguir para a página de Termo de Aceite
-    cy.get('[data-cy="termoDeAceite"]').then(el => {
-      const editor = (el[0] as any).ckeditorInstance; // Assert type as any to bypass TS error
-      editor.setData('Termo de Aceite Grupo-12 E.M. 012/2025 Eduã - Emerson'); // Preenche o campo "Termo de Aceite"
-    }); // Add the missing closing bracket and parenthesis
-
-    cy.get('[data-cy="texto-do-edital"]').click(); //Clica na aba Texto do Edital para seguir para a página de Texto do Edital
-    cy.get('[data-cy="texto"]').then(el => {
-      const editor = (el[0] as any).ckeditorInstance; // Assert type as any to bypass TS error
-      editor.setData('Texto do Edital Grupo-12 E.M. 012/2025 Eduã - Emerson'); // Preenche o campo "Texto do Edital"
-    }); // Add the missing closing bracket and parenthesis
-
-    cy.get('[data-cy="abrangencia"]').click(); //Clica na aba Abrangência para seguir para a página de Abrangência
-    cy.get('[data-cy="estado-mato-grosso-do-s"]').click(); //Clica no estado "Mato Grosso do Sul" para selecionar a Abrangência do Edital
-    cy.get('[data-cy="estado-mato-grosso"]').click(); //Clica no estado "Mato Grosso" para selecionar a Abrangência do Edital
-
-    cy.get('[data-cy="cronograma"]').click(); //Clica na aba Cronograma para seguir para a página de Cronograma
-    cy.get('[data-cy="periodo-de-submissao"]').click(); //Clica na aba Período de Submissão para seguir para a página de Período de Submissão
-    cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar um novo Período de Submissão
-    cy.get('[data-cy="chamadaUnsaved.inicio"]').type(getCurrentDateTime()); //Preenche o campo "Início" do Período de Submissão com a data do dia de hoje
-    cy.get('[data-cy="chamadaUnsaved.termino"]').type(
-      getCurrentDateTime({ addYears: 1 }),
-    ); //Preenche o campo "Término" do Período de Submissão com a data do dia de hoje + 1 ano
-    cy.get('[data-cy="chamada-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações do Período de Submissão
-    cy.get('[data-cy="orcamento"]').click(); //Clica na aba Orçamento para exibir as opções de Orçamento
-    cy.get('[data-cy="programa"]').click(); //Clica em Programa para seguir para a página de Programa
-    cy.get('[data-cy="programaId"]').click(); //Clica no campo de seleção de Programa
-    cy.get('[data-cy-index="programaId-item-0"]').click(); //Seleciona o primeiro Programa da lista de Programas
-   
-    cy.get('[data-cy="perguntas"]').click(); //Clica na aba Perguntas para seguir para a página de Perguntas
-    cy.get('[data-cy="indicadores-de-producao"]').click(); //Clica na aba Indicadores de Produção para seguir para a página de Indicadores de Produção
-    
-    cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar um novo Indicador de Produção
-    cy.get('[data-testid="ArrowDropDownIcon"]').click(); //Clica no campo de seleção de Indicador de Produção
-    cy.get('[data-cy="indicador-de-pro"]').click(); //Seleciona o Indicador de Produção "Número de artigos publicados em periódicos indexados"
-    cy.get('[data-cy="indicadorProducao-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações do Indicador de Produção
-    
-    cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar um novo Indicador de Produção
-    cy.get('[data-testid="ArrowDropDownIcon"]').click(); //Clica no campo de seleção de Indicador de Produção
-    cy.get('[data-cy="producao-bibliog"]').click(); //Seleciona o Indicador de Produção "Produção Bibliográfica"
-    cy.get('[data-cy="indicadorProducao-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações do Indicador de Produção
-    
-    cy.get('[data-cy="add-button"]').click(); //Clica no botão "Adicionar" para criar um novo Indicador de Produção
-    cy.get('[data-testid="ArrowDropDownIcon"]').click(); //Clica no campo de seleção de Indicador de Produção
-    cy.get('[data-cy="producao-tecnica"]').click(); //Seleciona o Indicador de Produção "Produção Técnica"
-    cy.get('[data-cy="indicadorProducao-confirmar"]').click(); //Clica no botão "Salvar" para salvar as informações do Indicador de Produção
-    
-
-    cy.get('[data-cy="menu-salvar"]').click(); //Clica no botão "Salvar" para salvar as informações do Edital
-    cy.get('[data-cy="menu-finalizar"]').click(); //Clica no botão "Finalizar" para salvar e sair da área de adição do Edital
-
-    //Resultado esperado: O Edital deve ser salvo com sucesso e o usuário deve ser redirecionado para a página de Editais.
+      'https://novo-sig.ledes.net/login',
+      'grupo12_gestor@sig.com',
+      'Grupo12@sig'
+    );
   });
 
+  it.only('Realiza login no sistema e cria um edital medio', () => {
+    cy.get('[data-cy="nav-group-edital"]').click();
+    cy.get('[data-cy="nav-item-publicar-edital"]').click();
+    cy.get('[data-cy="add-publicar-edital"]').click();
+
+    // nome do Edital
+    cy.get('[data-cy="nome"]').type('Grupo-12 E.M. 001/2025 Eduã-Emerson', { delay: 0 },);
+
+    // Restrições
+    cy.get('[data-cy="restricoes"]').click();
+    // Definir Duração do Projeto em Meses
+    cy.get('[data-cy="definirDuracaoProjetoEmMeses"]').check();
+    cy.get('[data-cy="duracaoProjetoEmMeses"]').type('8');
+    // Pesquisador pode submeter várias propostas
+    cy.get('[data-cy="pesquisadorSubmeterVariasPropostas"]').check();
+
+
+    // próxima seção
+    cy.contains('Próximo').click();
+
+    // Texto do Termo
+    cy.get('[data-cy="termoDeAceite"]')
+      .click()
+      .realType('Termo de Aceite Grupo-12 E.M. 001/2025 Eduã-Emerson', { delay: 0 },);
+
+    // próxima seção
+    cy.contains('Próximo').click();
+    
+    
+    // Texto do Edital
+    cy.get('[data-cy="texto"]')
+      .click()
+      .realType('Texto do Edital Grupo-12 E.M. 001/2025 Eduã-Emerson', { delay: 0 },);
+
+    // próxima seção
+    cy.contains('Próximo').click();
+
+    // Abrangência
+    cy.get('[data-cy="abrangencia"]').click();
+    cy.get('[data-cy="estado-mato-grosso-do-s"]').click(); 
+    cy.get('[data-cy="estado-mato-grosso"]').click(); 
+
+    // Período de Submissão
+    cy.get('[data-cy="cronograma"]').click();
+    cy.get('[data-cy="periodo-de-submissao"]').click();
+    cy.get('[data-cy="add-button"]').click();
+    cy.get('[data-cy="chamadaUnsaved.inicio"]').type(getCurrentDateTime());
+    cy.get('[data-cy="chamadaUnsaved.termino"]').type(getCurrentDateTime({ addYears: 1 }));
+    cy.get('[data-cy="chamada-confirmar"]').click();
+
+    // Orçamento > Programa
+    cy.get('[data-cy="orcamento"]').click();
+    cy.get('[data-cy="programa"]').click();
+    cy.get('[data-cy="programaId"]').click();
+    cy.get('[data-cy-index="programaId-item-0"]').click();
+
+    // Perguntas > Indicadores de Produção
+    cy.get('[data-cy="perguntas"]').click();
+    cy.get('[data-cy="indicadores-de-producao"]').click();
+    cy.get('[data-cy="add-button"]').click();
+    cy.get('[data-cy="indicadorProducaoUnsaved.id"]').click();
+    cy.get('[data-cy="producao-bibliog"]').click();
+    cy.get('[data-cy="indicadorProducao-confirmar"]').click();
+    cy.get('[data-cy="add-button"]').click();
+    cy.get('[data-cy="indicadorProducaoUnsaved.id"]').click();
+    cy.get('[data-cy="producao-tecnica"]').click();
+    cy.get('[data-cy="indicadorProducao-confirmar"]').click();
+    cy.get('[data-cy="add-button"]').click();
+    cy.get('[data-cy="indicadorProducaoUnsaved.id"]').click();
+    cy.get('[data-cy="indicador-de-pro"]').click();
+    cy.get('[data-cy="indicadorProducao-confirmar"]').click();
+
+    // Finalizar
+    cy.get('[data-cy="menu-salvar"]').click();
+    cy.get('[data-cy="menu-finalizar"]').click();
+
+  });
 });
